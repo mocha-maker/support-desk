b0efba129d447dbad10f138d888562f2ff7f9465
mocha-maker <flynn.pyc@gmail.com>
2022-04-27 15:53:52 -0400

    Update README, add Header, and Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
+// Utilities
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Components
+import Header from './components/Header';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
-  return (
-    <div className="App">
-      My App
-    </div>
-  );
+  return <>
+    <Router>
+      <div className="container">
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
+  </>
 }
 
 export default App;
