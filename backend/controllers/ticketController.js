07e1844239ed0f5a50a001c55642bc20a3d96440
mocha-maker <flynn.pyc@gmail.com>
2022-04-29 14:18:49 -0400

    Develop createTicket and getTickets functions

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -11,20 +11,50 @@ const Ticket = require('../models/ticketModel')
 // Colors
 const { red } = require('colors')
 
-// @desc Get User tickets
-// @route GET /api/tickets
-// @access Private
-const getTickets = expressAsyncHandler(async (req, res) => {
-
-  res.status(200).json({message: 'getTickets'})
-})
-
 // @desc Create a new ticket
 // @route POST /api/tickets
 // @access Private
 const createTicket = expressAsyncHandler(async (req, res) => {
+  const { product, description } = req.body
+  
+  if (!product || !description) {
+    res.status(400)
+    throw new Error ('Please add a product and description')
+  }
 
-  res.status(201).json({message: 'createTicket'})
+  // TODO: Refactor get user function
+  // Get user from JWT id
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.create({ 
+    product,
+    description,
+    user: req.user.id,
+   })
+
+  res.status(201).json(ticket)
+})
+
+// @desc Get User tickets
+// @route GET /api/tickets
+// @access Private
+const getTickets = expressAsyncHandler(async (req, res) => {
+  // Get user from JWT id
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id })
+
+  res.status(200).json(tickets)
 })
 
 module.exports = {
